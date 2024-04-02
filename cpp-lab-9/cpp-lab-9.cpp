d6d8d651e7accfd8525b8abe6455f202b929e453
Майя Рембо <maya.ossamu@gmail.com>
2024-04-02 23:03:28 +0300

    Добавьте файлы проекта.

---
+++ cpp-lab-9/cpp-lab-9.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+
+
+template<typename T>
+T sumelements(T array[], int size)
+{
+    T sum = 0;
+    for (int i = 0; i < size; ++i)
+    {
+        sum += array[i];
+    }
+    return sum;
+}
+
+int main()
+{
+    int intarray[] = { 1, 2, 3, 6 ,7, 9 };
+    double doublearray[] = { 1.4, 4.7, 3.86, 7.378 };
+
+    int intsum = sumelements(intarray, 6);
+    double doublesum = sumelements(doublearray, 4);
+
+    cout << "Sum of integers: " << intsum << endl;
+    cout << "Sum of doubles: " << doublesum << endl;
+}
